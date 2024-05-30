68ce5d59f0fc2a637b547057ab4610efe7577e3b
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 <80960745+woolozzx21@users.noreply.github.com>
2024-05-30 09:59:25 +0900

    Search_bs

--- book_junitTest/src/book_junitTest/BookTest.java
+++ book_junitTest/src/book_junitTest/BookTest.java
@@ -14,14 +14,15 @@ class BookTest {
 		bookLibrary = new Book();
 	}
 
-
+	// Test addBook()
 	@Test
     public void testAddBook() {
         boolean result = bookLibrary.addBook("1", "Effective Java", "Joshua Bloch", 2008);
         assertTrue(result);
         System.out.println("test add book passed");
     }
-
+	
+	// Test searchBook()
     @Test
     public void testSearchBook() {
         bookLibrary.addBook("1", "Effective Java", "Joshua Bloch", 2008);
@@ -30,7 +31,6 @@ class BookTest {
         
         System.out.println("test search book passed");
     }
-
     @Test
     public void testSearchBookNotFound() {
         String result = bookLibrary.searchBook("Nonexistent Book");
@@ -38,7 +38,8 @@ class BookTest {
         
         System.out.println("testSearchBookNotFound passed");
     }
-
+    
+    // Test removeBook()
     @Test
     public void testRemoveBook() {
         bookLibrary.addBook("1", "Effective Java", "Joshua Bloch", 2008);
@@ -47,13 +48,34 @@ class BookTest {
         
         System.out.println("testRemoveBook passed");
     }
-
     @Test
     public void testRemoveBookNotFound() {
     	boolean result = bookLibrary.removeBook("Nonexistent Book");
     	assertFalse(result);
         
         System.out.println("testRemoveBookNotFound passed");
+    }
+    
+    // Test search_bs()
+    @Test
+    public void testSearchBookById() {
+        bookLibrary.addBook("1", "Effective Java", "Murakami Haruki", 2011);
+        bookLibrary.addBook("2", "Effective Python", "Don Mills", 2002);
+        bookLibrary.addBook("3", "Effective C++", "Camil aloha", 2004);
+        bookLibrary.addBook("4", "Effective Go", "Joshua Bloch", 2024);
+        
+        String result = bookLibrary.search_bs("3");
+        assertTrue(result.contains("Effective C++"));
+        
+        System.out.println("testSearchBookById passed");
+    }
+
+    @Test
+    public void testSearchBookByIdNotFound() {
+        String result = bookLibrary.search_bs("5");
+        assertEquals("NO INFO", result);
+        
+        System.out.println("testSearchBookByIdNotFound passed");
     }
 
 }
