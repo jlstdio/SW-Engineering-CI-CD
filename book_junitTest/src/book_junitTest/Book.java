fc945a6fdf5ce486b452fde81c0fc23595b90ef7
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 <80960745+woolozzx21@users.noreply.github.com>
2024-06-02 15:20:59 +0900

    complete search_bs

--- book_junitTest/src/book_junitTest/Book.java
+++ book_junitTest/src/book_junitTest/Book.java
@@ -1,54 +1,64 @@
-
 package book_junitTest;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 // Book.java
 public class Book {
-    private List <IndiBook> bookList;
+    private List<IndiBook> bookList;
 
     public Book() {
         this.bookList = new ArrayList<>();
     }
 
-    private class IndiBook {
-        private String id;
+    public class IndiBook {
+        private int id;
         private String title;
         private String author;
         private int year;
 
-        public IndiBook(String id, String title, String author, int year) {
+        public IndiBook(int id, String title, String author, int year) {
             this.id = id;
             this.title = title;
             this.author = author;
             this.year = year;
         }
-       
+
         public String getTitle() {
-        	return this.title;
+            return this.title;
+        }
+
+        public int getId() {
+            return this.id;
         }
 
         public String getInfo() {
-        	return "IndiBook{" +
-                    "id='" + this.id + '\'' +
+            return "IndiBook{" +
+                    "id=" + this.id +
                     ", title='" + this.title + '\'' +
                     ", author='" + this.author + '\'' +
                     ", year=" + this.year +
                     '}';
         }
-
     }
-    
+
     // Manage Book
     // Add Book
-    public boolean addBook(String id, String title, String author, int year) {
+    public boolean addBook(int id, String title, String author, int year) {
         IndiBook newBook = new IndiBook(id, title, author, year);
         bookList.add(newBook);
-        Collections.sort(bookList, Comparator.comparing(IndiBook::getId)); // Sort by Id When add a book
+        // sort when add book
+        Collections.sort(bookList, new Comparator<IndiBook>() {
+            @Override
+            public int compare(IndiBook book1, IndiBook book2) {
+                return Integer.compare(book1.getId(), book2.getId());
+            }
+        });
         return true;
     }
+
     // Search Book
     public String searchBook(String title) {
         for (IndiBook book : bookList) {
@@ -58,6 +68,7 @@ public class Book {
         }
         return "NO INFO";
     }
+
     // Remove Book
     public boolean removeBook(String title) {
         for (IndiBook book : bookList) {
@@ -68,25 +79,26 @@ public class Book {
         }
         return false;
     }
+
     // Search Book by book's code (binary search)
-    public String search_bs(String id) {
-    	int low = 0; int mid = 0;
-    	int high = bookList.size() - 1;
-    	while(low<=high) {
-    		int mid = (low+high) / 2;
-    		IndiBook midbook = bookList.get(mid);
-    		int cmp = midBook.getId().compareTo(id);
-    		
-    		if(cmp < 0) {
-    			low = mid + 1;
-    		}
-    		else if (cmp > 0) {
-    			high = mid - 1;
-    		}
-    		else {
-    			return midBook.getInfo();
-    		}
-    	}
-    	return "NO INFO";
+    public String search_bs(int id) {
+        // Sort the list before performing binary search
+       
+        int low = 0;
+        int high = bookList.size() - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            IndiBook midBook = bookList.get(mid);
+            int cmp = Integer.compare(midBook.getId(), id);
+
+            if (cmp < 0) {
+                low = mid + 1;
+            } else if (cmp > 0) {
+                high = mid - 1;
+            } else {
+                return midBook.getInfo();
+            }
+        }
+        return "NO INFO";
     }
 }
