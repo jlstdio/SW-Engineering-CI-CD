93ad155ab4a17033691a49224ab9c105747b7333
LeeJoonHee <neo81389@gmail.com>
2024-06-15 23:41:10 +0900

    fix : class name fix

--- book_junitTest/src/book_junitTest/BookPerformance.java
+++ book_junitTest/src/book_junitTest/BookPerformance.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class Performance_test {
+public class BookPerformance {
     
 	private Book bookLibrary;
 	
